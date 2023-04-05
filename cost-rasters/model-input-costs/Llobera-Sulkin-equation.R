fd028a41fbb7978f50e57acb2dc3958084b89ad0
cocoemily <ec3307@nyu.edu>
2023-04-05 16:52:17 -0400

    updates to the cost raster creation in terms of multiple for desert and river crossings

---
+++ cost-rasters/model-input-costs/Llobera-Sulkin-equation.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+
+LS_cost = function(s) {
+  return(
+    2.635 + (17.37 * s) + (42.37 * s^2) - (21.43 * s^3) + (14.93 * s^4)
+  )
+}
+
+LS_cost(0) #0 degrees
+LS_cost(0.2679) #15 degrees
+
+(LS_cost(0.2679) - LS_cost(0))/LS_cost(0.2679)
