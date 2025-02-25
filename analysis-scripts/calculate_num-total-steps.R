afaa13d69ea4e6075fffb5f8efaab483fc6bb9ae
cocoemily <ec3307@nyu.edu>
2025-02-25 11:08:56 -0500

    updates to Neanderthal sites for creating maps

---
+++ analysis-scripts/calculate_num-total-steps.R
@@ -0,0 +1,85 @@
+library(tidyverse)
+library(here)
+
+steps = data.frame(start = character(0),
+                   period = character(0),
+                   route = double(0),
+                   max.step = double(0))
+
+for(start in c("Caucasus-north", "Caucasus-south")) {
+  
+  # Assign the folder in which all the individual simulation output files are located 
+  my_dir = paste0(getwd(),"/outputs/", start)
+  
+  # Create a list of all the file names in the identified folder
+  all_files = list.files(path = my_dir, all.files = TRUE, full.names = TRUE, pattern = "\\.csv$")
+  MIS3 = all_files[str_detect(all_files, "MIS3")]
+  MIS4s = all_files[str_detect(all_files, "MIS4-small-Caspian")]
+  MIS4b = all_files[str_detect(all_files, "MIS4-big-Caspian")]
+  MIS5a = all_files[str_detect(all_files, "MIS5a")]
+  MIS5bl = all_files[str_detect(all_files, "MIS5b-low-water")]
+  MIS5bh = all_files[str_detect(all_files, "MIS5b-high-water")]
+  MIS5c = all_files[str_detect(all_files, "MIS5c")]
+  MIS5dl = all_files[str_detect(all_files, "MIS5d-low-water")]
+  MIS5dh = all_files[str_detect(all_files, "MIS5d-high-water")]
+  MIS5e = all_files[str_detect(all_files, "MIS5e")]
+  MIS6s = all_files[str_detect(all_files, "MIS6-small-Kara")]
+  MIS6b = all_files[str_detect(all_files, "MIS6-big-Kara")]
+  
+  timeperiods = list("MIS3" = MIS3, 
+                     "MIS4s" = MIS4s, "MIS4b" = MIS4b,
+                     "MIS5a" = MIS5a, 
+                     "MIS5bl" = MIS5bl, "MIS5bh" = MIS5bh,
+                     "MIS5c" = MIS5c, 
+                     "MIS5dl" = MIS5dl, "MIS5dh" = MIS5dh,
+                     "MIS5e" = MIS5e,
+                     "MIS6s" = MIS6s, "MIS6b" = MIS6b)
+  names(timeperiods)
+  
+  for(t in 1:length(timeperiods)) {
+    period = names(timeperiods)[[t]]
+    print(period)
+    files = timeperiods[[t]]
+    files = files[str_detect(files, "LIST")]
+    
+    if(length(files) != 0) {
+      list_size = length(files)
+      
+      # Iterate over all the files located in the given folder 
+      for(l in 1:list_size[1]){
+        # Reformat the name of the files so that it can be used later 
+        file.name = strsplit(files[l],"/")
+        file.name = unlist(file.name)
+        name.size = length(file.name)
+        new.file = file.name[name.size]
+        
+        # Separate out the components of the file name 
+        filename.split = strsplit(new.file,"_") 
+        filename.split = unlist(filename.split)
+        origin = gsub("\\)|\\(", "", filename.split[4]) # Origin of the run 
+        #goal = gsub("\\)|\\(", "", filename.split[5])
+        
+        patch_res_km = as.numeric(filename.split[7])
+        time_period = filename.split[5]
+        
+        # Import the data without headers
+        ds <- read.table(paste(my_dir, "/", new.file,sep=""), fill = TRUE, skip = 19, stringsAsFactors = FALSE, sep = ",")
+        
+        # Keep only the coordinates of the paths 
+        #ds <- ds[,c(2,6)]
+        # Change the names and reduce the floats coordinates to integers 
+        colnames(ds) <- c("x","y")
+        ds$x <- as.integer(ds$x)
+        ds$y <- as.integer(ds$y)
+        
+        ds = ds[,c(1,2)]
+        ds$step <- as.numeric(rownames(ds))
+        
+        dat.final <- ds
+        
+        steps[nrow(steps) + 1, ] = c(start, period, l, max(dat.final$step))
+      }
+    }
+  }
+}
+
